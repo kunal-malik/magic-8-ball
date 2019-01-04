1c1bf67d36f62b64e45b943a43b89b59838e27f2
VARIANT\kunal.malik <kunal.malik@freestyleiot.com>
2019-01-04 13:10:08 +1100

    Improvement in styles and scripts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/style.scss';
 import App from './components/App';
-//TODO - scss variables and modularization
 //TODO - test in ui and backend
 //TODO - animation while showing a response
 ReactDOM.render(<App />, document.getElementById('root'));
